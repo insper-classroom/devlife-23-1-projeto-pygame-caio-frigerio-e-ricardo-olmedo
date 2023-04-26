e8091cb93bba0391588cf7625506162842ccd72c
Ricardo <ricardolins166@gmail.com>
2023-04-26 11:52:19 -0300

    criacao da tela de game over

--- docs/jogo.py
+++ docs/jogo.py
@@ -1,12 +1,19 @@
 from telainicio import *
+from telagameover import *
 from Funcoes import *
+
 ponto = Ponto()
 tela_inicio = tela_inicio()
+tela_over = gameover()
 if __name__ == '__main__':
-    tela_inicio.desenha()
-    ponto.coracao_spw()
-    while ponto.recebe_evento(ponto.state['tecla']):
-        ponto.colisao()
-        ponto.desenha_coracao()
-        ponto.desenha()
-        
+    rodando= True
+    while rodando:
+        tela_inicio.desenha()
+        ponto.coracao_spw()
+        while ponto.recebe_evento(ponto.state['tecla']):
+            ponto.colisao()
+            ponto.desenha_coracao()
+            ponto.desenha()
+        tela_over.desenha()
+        if tela_over.recebe_evento()==True or ponto.recebe_evento(ponto.state['tecla'])==False:
+            rodando = False
