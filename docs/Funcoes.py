b3b3e275dbbc8d48db3ee34cb00766a40aa0efd3
caio <caioliberal@gmail.com>
2023-05-02 12:18:37 -0300

    Ajuste, inicio do contador de pontos

--- docs/Funcoes.py
+++ docs/Funcoes.py
@@ -8,31 +8,54 @@ class SNAKE:
 		self.new_block = False
 		self.remove_blocks = False
 		imagemup = pygame.image.load('sprites/assets_dragao/dragaoo_cab (4).png').convert_alpha()
+		
 		imagemdown = pygame.image.load('sprites/assets_dragao/dragaoo_cab (2).png').convert_alpha()
 		imagemright=pygame.image.load('sprites/assets_dragao/dragaoo_cab (5).png').convert_alpha()
 		imagemleft = pygame.image.load('sprites/assets_dragao/dragaoo_cab (3).png').convert_alpha()
+        
+
+        
 		self.head_up = pygame.transform.scale(imagemup, (90, 80))
 		self.head_down = pygame.transform.scale(imagemdown, (90, 80))
 		self.head_right = pygame.transform.scale(imagemright, (80, 90))
 		self.head_left = pygame.transform.scale(imagemleft, (80, 90))
 		
 		rabobaixo= pygame.image.load('sprites/assets_dragao/rabobaixo removebg.png').convert_alpha()
+		size_ww = rabobaixo.get_width()
+		size_hh = rabobaixo.get_height()
 		rabocima = pygame.image.load('sprites/assets_dragao/rabocima-removebg-preview.png').convert_alpha()
 		raboesquerda = pygame.image.load('sprites/assets_dragao/raboesqeurda-removebg-preview.png').convert_alpha()
 		rabodireita = pygame.image.load('sprites/assets_dragao/rabodireita-removebg-preview.png').convert_alpha()
-		self.tail_up =  pygame.transform.scale(rabobaixo, (70, 70))
-		self.tail_down = pygame.transform.scale(rabocima, (70, 70))
-		self.tail_right = pygame.transform.scale(raboesquerda, (70, 70))
-		self.tail_left = pygame.transform.scale(rabodireita, (70, 70))
-
-		self.body_vertical = pygame.image.load('sprites/assets_dragao/dragao_corpo (5).png').convert_alpha()
-		self.body_horizontal = pygame.image.load('sprites/assets_dragao/dragao_corpo (4).png').convert_alpha()
+		self.tail_up =  pygame.transform.scale(rabobaixo, (size_ww/3, size_hh/3))
+		self.tail_down = pygame.transform.scale(rabocima, (size_ww/3, size_hh/3))
+		self.tail_right = pygame.transform.scale(raboesquerda, (size_ww/3, size_hh/3))
+		self.tail_left = pygame.transform.scale(rabodireita, (size_ww/3, size_hh/3))
 
 		
-		self.body_tr = pygame.image.load('sprites/assets_dragao/dragao_corpo_vira_e.png').convert_alpha()
-		self.body_tl = pygame.image.load('sprites/assets_dragao/dragao_corpo_vira_e.png').convert_alpha()
-		self.body_br = pygame.image.load('sprites/assets_dragao/dragao_corpo_vira_d.png').convert_alpha()
-		self.body_bl = pygame.image.load('sprites/assets_dragao/dragao_corpo_vira_d.png').convert_alpha()
+        
+		corpo_baixo = pygame.image.load('sprites/assets_dragao/dragao_corpo (2).png').convert_alpha()
+		corpo_horizontal_direita = pygame.image.load('sprites/assets_dragao/dragao_corpo (5).png').convert_alpha()
+		corpo_horizontal_esquerda = pygame.image.load('sprites/assets_dragao/dragao_corpo (3).png').convert_alpha()
+		corpo_vertical_cima = pygame.image.load('sprites/assets_dragao/dragao_corpo (4).png').convert_alpha()
+		size_corpo_h = corpo_baixo.get_height()
+		size_corpo_w = corpo_baixo.get_width()
+		self.body_vertical_baixo = pygame.transform.scale(corpo_baixo, (size_corpo_w/3, size_corpo_h/3))
+		self.body_vertical_cima = pygame.transform.scale(corpo_vertical_cima, (size_corpo_w/3, size_corpo_h/3))
+		
+		self.body_horizontal = pygame.transform.scale(corpo_horizontal_direita, (size_corpo_w/3, size_corpo_h/3))
+		self.body_horizontal_e = pygame.transform.scale(corpo_horizontal_esquerda, (size_corpo_w/3, size_corpo_h/3))
+		
+		body_tr = pygame.image.load('sprites/assets_dragao/dragao_corpo_vira_d.png').convert_alpha()
+		body_tl = pygame.image.load('sprites/assets_dragao/dragao_corpo_vira_e.png').convert_alpha()
+		body_br = pygame.image.load('sprites/assets_dragao/dragao_corpo_vira_d.png').convert_alpha()
+		body_bl = pygame.image.load('sprites/assets_dragao/dragao_corpo_vira_e.png').convert_alpha()
+		size_corpo_turn_w = body_tr.get_width()
+		size_corpo_turn_h = body_tr.get_height()
+		
+		self.body_tr = pygame.transform.scale(body_tr, (size_corpo_turn_w/5, size_corpo_turn_h/5))
+		self.body_tl = pygame.transform.scale(body_tl, (size_corpo_turn_w/5, size_corpo_turn_h/5))
+		self.body_br = pygame.transform.scale(body_br, (size_corpo_turn_w/5, size_corpo_turn_h/5))
+		self.body_bl = pygame.transform.scale(body_bl, (size_corpo_turn_w/5, size_corpo_turn_h/5))
 
 
 		
@@ -42,9 +65,9 @@ class SNAKE:
 		self.update_tail_graphics()
 
 		for index,block in enumerate(self.body):
-			x_pos = int(block.x * cell_size)
+			x_pos = int(block.x * cell_size )
 			y_pos = int(block.y * cell_size)
-			block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+			block_rect = pygame.Rect(x_pos ,y_pos,cell_size,cell_size)
 
 			if index == 0:
 				screen.blit(self.head,block_rect)
@@ -53,21 +76,27 @@ class SNAKE:
 			else:
 				previous_block = self.body[index + 1] - block
 				next_block = self.body[index - 1] - block
-				if previous_block.x == next_block.x:
-					screen.blit(self.body_vertical,block_rect)
-				elif previous_block.y == next_block.y:
+				if previous_block.x == next_block.x and previous_block.y <= next_block.y:
+					screen.blit(self.body_vertical_baixo,block_rect)
+				if previous_block.x == next_block.x and previous_block.y >= next_block.y:
+					screen.blit(self.body_vertical_cima,block_rect)
+				elif previous_block.y == next_block.y and previous_block.x <= next_block.x:
 					screen.blit(self.body_horizontal,block_rect)
+				elif previous_block.y == next_block.y and previous_block.x >= next_block.x:
+					screen.blit(self.body_horizontal_e,block_rect)
 				else:
 					if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
-						screen.blit(self.body_tl,block_rect)
+						screen.blit(self.body_horizontal_e,block_rect)
 					elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
-						screen.blit(self.body_bl,block_rect)
+						screen.blit(self.body_horizontal_e,block_rect)
 					elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
-						screen.blit(self.body_tr,block_rect)
+						screen.blit(self.body_horizontal,block_rect)
 					elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
-						screen.blit(self.body_br,block_rect)
+						screen.blit(self.body_horizontal,block_rect)
 
 	def update_head_graphics(self):
+		if len(self.body) == 0:
+			print('game over')
 		head_relation = self.body[1] - self.body[0]
 		if head_relation == Vector2(1,0): self.head = self.head_left
 		elif head_relation == Vector2(-1,0): self.head = self.head_right
@@ -146,19 +175,18 @@ class GUARDA:
 		self.barra_33 = pygame.transform.scale(barra_33,(size_w/4 ,size_h/4))
 		self.barra_0 = pygame.transform.scale(barra_100,(size_w/4 ,size_h/4))
 		self.vivo = True
-
-
-	def draw_monster(self):
-		guarda_react = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
-		if self.vivo == True:
-			screen.blit(guarda,(guarda_react.x, guarda_react.y))
-		
+    	
 	def randomize(self):
 
 		self.x = random.randint(0,cell_number - 3)
 		self.y = random.randint(0,cell_number - 3)
 		self.pos = Vector2(self.x,self.y)
 
+	def draw_monster(self):
+		guarda_react = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
+		if self.vivo == True:
+			screen.blit(guarda,(guarda_react.x, guarda_react.y))
+		
 	def barra_vida(self):
 		guarda_react_bara = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
 		if self.vida == 2:
@@ -168,7 +196,9 @@ class GUARDA:
 		if self.vida == 1:
 			screen.blit(self.barra_33,(guarda_react_bara.x - 104,guarda_react_bara.y + 50))
 		
-
+class PONTO:
+	def __init__(self):
+		self.pontos = 0
 
 class TEXTO:
 	def __init__(self):
@@ -187,7 +217,8 @@ class MAIN:
 		self.fruit = FRUIT()
 		self.guarda = GUARDA()
 		self.msg = TEXTO()
-		self.msg.textos = (f'Wave 1, Mate o guarda!')
+		self.pontos = PONTO()
+		self.msg.textos = (f'Mate o guarda! & faca pontos!')
 		self.fase = 1
 		self.fase_2 = pygame.image.load('sprites/fundo2.png').convert_alpha()
 	def update(self):
@@ -202,10 +233,8 @@ class MAIN:
 		self.msg.imprimir()
 		self.guarda.barra_vida()
 
-	def telas(self):
-		if self.fase == 2:
-			screen.blit(self.fase_2,(0,0))
-		
+
+
 	def game_over(self):
 		self.snake.reset()
 
@@ -214,17 +243,20 @@ class MAIN:
 			self.fruit.randomize()
 			self.snake.add_block()
 			self.snake.play_crunch_sound()
+			self.pontos.pontos += 10
 		
 		if self.snake.body[0] == self.guarda.pos:
 			print(self.snake.body[0], '', self.guarda.pos)
 			self.guarda.vida -=1
-			self.msg.textos = (f'O GUARDA TEM {self.guarda.vida} de vida')
+			
 			print(self.guarda.vida)
 			self.snake.remove_block()
 			if self.guarda.vida == 0:
-				
+				self.pontos.pontos *=2
 				self.fase = 2
-				self.guarda.vivo = False
+				self.guarda.randomize()
+				self.guarda.vida = 3
+				self.guarda.vivo = True
 			
 		for block in self.snake.body[1:]:
 			if block == self.fruit.pos:
