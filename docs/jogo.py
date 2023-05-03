a24e3ea7a444eeb0ee6598781a1f834d3c99292b
Ricardo <ricardolins166@gmail.com>
2023-05-02 23:43:44 -0300

    JOGO E SITE FINAL

--- docs/jogo.py
+++ docs/jogo.py
@@ -1,9 +1,7 @@
 from telainicio import *
-from telagameover import *
 rodar = True
 tela_inicioa = tela_inicio()
 tela_inicioa.desenha()
-gameovers=gameover()
 while rodar ==True:
     if tela_inicioa.rodando == False:
         import Funcoes 
